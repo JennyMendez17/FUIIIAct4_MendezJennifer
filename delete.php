ca83d04a7e209f1606c9aed1c907f63c005850ea
Mendez Alvarez Jennifer Yadira <91636137+JennyMendez17@users.noreply.github.com>
2021-11-20 01:43:15 -0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+
+include("conexion.php");
+$con=conectar();
+
+$nombre=$_POST['nombre'];
+$apellidos=$_POST['apellidos'];
+$ventas=$_POST['ventas'];
+$inventario=$_POST['inventario'];
+$horas=$_POST['horas'];
+
+$sql="DELETE FROM empleado SET  nombre='$nombre',apellidos='$apellidos',ventas='$ventas',inventario='$inventario',horas='$horas' ";
+
+
+$query=mysqli_query($con,$sql);
+
+    if($query){
+        Header("Location: index.php");
+    }
+?>
